f10d6b73ca3faa9648a432af8653bd4985c17c74
brondo138 <93743624+brondo138@users.noreply.github.com>
2025-03-01 09:37:08 -0600

    Update function.ts

--- ejercicio 1/function.ts
+++ ejercicio 1/function.ts
@@ -1,25 +1,24 @@
 import { GeneroLibro, libros } from "./libros";
 
-export function seleccionarLibro(opcion: any, lista: any[]) {
-    
-    let seleccion = lista.find(cliente => cliente.id === opcion);
+export function seleccionar(opcion: any, lista: any[]) {
+    let seleccion = lista.find(lista => lista.id === opcion);
     return seleccion;
 }
 
-export function mostrar(lista:any[],categoria?: GeneroLibro){
-
+export function mostrar(lista: any[], categoria?: GeneroLibro) {
     if (categoria) {
         let librosFiltrados = libros.filter(libro => libro.genero == categoria);
         librosFiltrados.forEach((dato, index) => {
-            console.log(`${index + 1}.${dato.nombre}`);
+            console.log(`${index + 1}. ${dato.nombre}`);
         });
-    }else {
+    } else {
         lista.forEach((dato, index) => {
-            console.log(`${index + 1}.${dato.nombre}`);
+            console.log(`${index + 1}. ${dato.nombre}`);
         });
     }
 }
 
-export function seleccionarLibros(filtrado,lista: any[]) {
-    let librosFiltrados = lista.filter(libros => libros.genero == filtrado);
+export function seleccionarLibrosGenero(filtrado: GeneroLibro, lista: any[]) {
+    let librosFiltrados = lista.filter(libro => libro.genero == filtrado);
+    return librosFiltrados;
 }
